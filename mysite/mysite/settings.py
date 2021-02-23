d5de5dafde6cd5abcb0b2323f7915d89c3ef81c0
ronlevy <ronlevy12000@gmail.com>
2021-02-23 12:33:22 +0200

    Procfile changed to main.wsgi

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +133,5 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
