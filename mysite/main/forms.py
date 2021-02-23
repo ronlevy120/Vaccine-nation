77f67d1a86b9aa69ab7cf2346f43eefa1bd74caf
ronlevy <ronlevy12000@gmail.com>
2021-02-23 17:08:42 +0200

    Back to CharField, text_area widget added

--- mysite/main/forms.py
+++ mysite/main/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class CreateNewList(forms.Form):
-    name = forms.TextField(label="Name", max_length=200, required=False)
+    name = forms.CharField(label="Name", max_length=200, required=False, widget=forms.Textarea)
     check = forms.BooleanField(required=False)
